d93fb1737e525dd0e19cc77651e4e91f93dd1384
Britosl1 <lucasbrito.carneiro@gmail.com>
2022-09-29 21:30:50 -0300

    feat - create projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Hero from './sections/Hero'
 import NavBar from './sections/NavBar'
 
 function App() {
-  return <NavBar />
+  return (
+    <>
+      <NavBar />
+      <Hero />
+    </>
+  )
 }
 
 export default App
---
+++ src/sections/Hero/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { HeroContainer } from './styles'
+
+const Hero: React.FC = () => {
+  return <HeroContainer>teste</HeroContainer>
+}
+
+export default Hero
