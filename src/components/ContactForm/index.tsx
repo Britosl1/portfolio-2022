71473337711834d73318d607cad8145a670ef5a2
Britosl1 <lucasbrito.carneiro@gmail.com>
2022-10-25 22:13:08 -0300

    feat - add emailJs

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
+import emailjs from '@emailjs/browser'
 
 import { AiOutlineClose } from 'react-icons/ai'
 import Button from '../Button'
@@ -12,7 +13,7 @@ interface IContactFormProps {
   onClose: () => void
 }
 
-interface IFormInputs {
+interface IFormInputs extends Record<string, unknown> {
   fullName: string
   email: string
   message: string
@@ -27,6 +28,7 @@ const formSchema = yup
   .required()
 
 const ContactForm: React.FC<IContactFormProps> = ({ onClose }) => {
+  const [emailText, setEmailText] = useState<string>('')
   const {
     register,
     handleSubmit,
@@ -34,11 +36,31 @@ const ContactForm: React.FC<IContactFormProps> = ({ onClose }) => {
   } = useForm<IFormInputs>({
     resolver: yupResolver(formSchema),
   })
-  const onSubmit = (data: IFormInputs) => console.log(data)
+
+  const sendEmail = (formData: IFormInputs) => {
+    // e.preventDefault()
+    emailjs
+      .send(
+        'service_nntplnd',
+        'template_rk4vxrw',
+        formData,
+        'YBCo3LFhyLOhmYRci'
+      )
+      .then(
+        (result) => {
+          setEmailText(result.text)
+        },
+        (error) => {
+          setEmailText(error.text)
+        }
+      )
+  }
+
+  // const onSubmit = (data: IFormInputs) => console.log(data)
 
   return (
     <BlurContainer>
-      <ContactFormContainer onSubmit={handleSubmit(onSubmit)}>
+      <ContactFormContainer onSubmit={handleSubmit(sendEmail)}>
         <AiOutlineClose id="close" onClick={onClose} size={18} />
         <label htmlFor="fullName">Name</label>
         <input type="text" id="fullName" {...register('fullName')} />
@@ -50,6 +72,7 @@ const ContactForm: React.FC<IContactFormProps> = ({ onClose }) => {
         <textarea rows={3} id="message" {...register('message')} />
         {errors.message && <p>Message is required</p>}
         <Button title="Send" secondary type="submit" />
+        <p>{emailText}</p>
       </ContactFormContainer>
     </BlurContainer>
   )
